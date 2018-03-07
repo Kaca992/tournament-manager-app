ecf9d4d58d7fc5adf8f125279267d4a0201446fc
kaca992 <kacavendadino@gmail.com>
2018-03-07 19:21:13 +0100

    initial setup

---
+++ tournament-manager-be/Tournament.Manager.Application/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Tournament.Manager.Application
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            // https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/enabling-cross-origin-requests-in-web-api
+            // Install-Package Microsoft.AspNet.WebApi.Cors
+            var cors = new EnableCorsAttribute("http://localhost:9000/", "*", "*");
+            config.EnableCors(cors);
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            // response to camelCase https://stackoverflow.com/questions/28552567/web-api-2-how-to-return-json-with-camelcased-property-names-on-objects-and-the
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
---
+++ tournament-manager-be/tournament-manager-be/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tournament_manager_be
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
