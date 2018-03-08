6c681becf8251f6a4500025a663c67730bda9437
kaca992 <kacavendadino@gmail.com>
2018-03-08 19:53:29 +0100

    - fix for cascading FK

---
+++ tournament-manager-be/Tournament.Manager.ConfigurationSetup/Interface/IDataStorageProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tournament.Manager.ConfigurationSetup.Interface
+{
+    public interface IDataStorageProvider
+    {
+        bool IsDataProviderInstalled();
+        void InstallDataProvider();
+    }
+}
