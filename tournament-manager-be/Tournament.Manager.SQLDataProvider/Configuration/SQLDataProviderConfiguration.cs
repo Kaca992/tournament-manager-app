582be0828213c73200467354a4c79d1870798bc9
kaca992 <kacavendadino@gmail.com>
2018-03-08 20:28:43 +0100

    added entity framework and started sql data provider

---
+++ tournament-manager-be/Tournament.Manager.SQLDataProvider/Configuration/SQLDataProviderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Manager.DataCommon.Configuration;
+
+namespace Tournament.Manager.SQLDataProvider.Configuration
+{
+    public class SQLDataProviderConfiguration : IDataStorageConfiguration
+    {
+        public void InstallDataProvider()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsDataProviderInstalled()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
